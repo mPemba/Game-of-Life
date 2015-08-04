7d7c629b08db6be478d27b4dd5693b0ec9c3830d
Michael Sayer <michael.j.say@gmail.com>
2015-08-04 16:20:07 -0600

    foundations

---
+++ life.js
@@ -0,0 +1,44 @@
+(function() {
+
+	var _ = self.life = function(seed) {
+		this.seed = seed;
+		this.height = seed.length;
+		this.width = seed[0];
+
+		this.prevBoard = [];
+		this.board = cloneArray(seed);
+	};
+
+	_.prototype = {
+		next: function() {
+
+		},
+		toString: function() {
+
+		}
+	};
+
+})();
+
+//Helpers
+//WARNING: this will only clone 2D arrays
+function cloneArray(array) {
+	return array.slice().map(function(row) {
+		return row.slice();
+	});
+}
+
+
+var game = new Life([
+	[0, 0, 0, 0, 0],
+	[0, 0, 1, 0, 0],
+	[0, 0, 1, 0, 0],
+	[0, 0, 1, 0, 0],
+	[0, 0, 0, 0, 0],
+]);
+
+console.log(game + '');
+
+game.next();
+
+console.log(game + '');
